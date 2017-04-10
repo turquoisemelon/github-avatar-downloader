1d92399f0d941f747f46a6ccd1587760786415ef
Meltem <melltemkilic@gmail.com>
2017-04-10 21:53:36 +0000

    http request has been done to github's api using request module.User agent header defined

--- download_avatars.js
+++ download_avatars.js
@@ -1,16 +1,32 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
-var GITHUB_USER = "turquoisemelon";
-var GITHUB_TOKEN = "c870aa54065387d127ffcc86d26f81c905e9e9b2";
+var GITHUB_USER = 'turquoisemelon';
+var GITHUB_TOKEN = 'c870aa54065387d127ffcc86d26f81c905e9e9b2';
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  var options = {
+    uri: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+
+  request.get(options, function(error, response, body) {
+
+    if (error) {
+      console.log('Got an error: ', error);
+      return;
+    }
+
+    if (response.statusCode == 200) {
+      console.log('Response Status Code: ', response.statusCode);
+      console.log('Options: ', options);
+      console.log('Got body: ', body);
+      cb(body);
+    }
+  })
 }
 
-getRepoContributors("nodejs", "node", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors('jquery', 'jquery', function(something) {
+  console.log('The body is: ', something);
 });
