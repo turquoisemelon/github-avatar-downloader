ad0dc5d13e0586326b77f6d77f9f6092b6bfdf8d
Meltem <melltemkilic@gmail.com>
2017-04-10 20:49:04 +0000

    API request URL has been constructed in the main js file and tested

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,12 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "turquoisemelon";
+var GITHUB_TOKEN = "c870aa54065387d127ffcc86d26f81c905e9e9b2";
+
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("nodejs", "node", function(err, result) {
